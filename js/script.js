17942325c5be8b2d93df4534d6a18eb89b6aa807
Suadabd <112632719+Suadabd@users.noreply.github.com>
2022-09-20 12:20:36 -0700

    Updating Repo name

--- js/script.js
+++ js/script.js
@@ -2,17 +2,11 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-
-
-
 /*
 For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-
-
-
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
@@ -71,13 +65,10 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 This section uses button elements and event listener to switch between relevent pages. 
 */
-
 function addPagination(list) {
    const numberOfPages = list.length / 9;
    const linkList = document.querySelector('.link-list');
    linkList.innerHTML = '';
-
-   // This loop creates the button elements
    for ( let i = 0; i < numberOfPages; i++) {
       const li = document.createElement('li');
       const button = document.createElement('button');
